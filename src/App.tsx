22a1570e9ad0e7e64f0d84693346052d32f3e8d7
shinmink <sinmk99@hansung.ac.kr>
2025-08-02 13:51:48 +0900

    FEAT : 어플 최상단에 쿠키 기반 정보 유지 로직 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
 // src/App.tsx
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router';
+import {useEffect, useState} from "react";
+import useUserStore from "./stores/userStore.ts";
 
 const App = () => {
+    const { isLoggedIn, user, fetchUserInfo } = useUserStore();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetchUserInfo().finally(() => setLoading(false));
+    }, []);
+    if (loading) return <div>로딩 중...</div>;
+
+    // if (!isLoggedIn) {
+    //     return (
+    //         <div>
+    //             <h1>로그인이 필요합니다.</h1>
+    //             <p>로그인 후 서비스를 이용할 수 있습니다.</p>
+    //         </div>
+    //     );
+    // }
+    // if (!user) {
+    //     return <div>사용자 정보를 불러오는 중...</div>;
+    // }
+
     return (
         <>
             <RouterProvider router={router} />
