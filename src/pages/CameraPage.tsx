dfe4583fdccea4c34d12a7d039f236eb4b1aed71
shinmink <sinmk99@hansung.ac.kr>
2025-08-16 13:35:08 +0900

    FIX : 주석처리

--- src/pages/CameraPage.tsx
+++ src/pages/CameraPage.tsx
@@ -37,7 +37,7 @@ const CameraPage = () => {
         videos,
         recording,
         lastRecordResult,
-        downloadUrl,
+        //downloadUrl,
         loading,
         error,
         fetchVideos,
